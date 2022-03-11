96028311f6944f0fdfd77d0b27ada964cf1dabcd
youssef sk <alsarakibiy@gmail.com>
2022-03-11 16:36:59 +0200

    add animation for stats-sec and skill-prog-sec

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 // Start Timer Event
+let currentYear = 2022;
 let daysEl = document.querySelector("[data-days]");
 let hoursEl = document.querySelector("[data-hours]");
 let minsEl = document.querySelector("[data-mins]");
 let secEl = document.querySelector("[data-sec]");
 setInterval(() => {
 	let currnetDate = new Date().getTime();
-	let EventDate = new Date("Dec 31 , 2022 23:59:59 ");
+	let EventDate = new Date(`Dec 31 , ${currentYear} 23:59:59 `);
 	deffDate = EventDate - currnetDate;
 	let days = Math.floor(deffDate / (24 * 60 * 60 * 1000));
 	let hours = Math.floor(deffDate / (60 * 60 * 1000) - days * 24);
@@ -18,6 +19,52 @@ setInterval(() => {
 	minsEl.innerHTML = mins < 10 ? "0" + mins : mins;
 	secEl.innerHTML = sec < 10 ? "0" + sec : sec;
 	if ((deffDate = 0)) {
-		clearInterval();
+		currentYear++;
 	}
 }, 1000);
+//  Count Animtion
+let sections = document.querySelectorAll("[data-count]");
+let container = document.querySelector("[data-section]");
+let staticsCounter = new IntersectionObserver(function (contain) {
+	if (contain[0].isIntersecting) {
+		console.log(contain[0]);
+		sections.forEach((el) => {
+			const maxNum = el.dataset.count;
+			const numInc = setInterval(() => {
+				el.textContent++;
+				if (el.textContent === maxNum) {
+					clearInterval(numInc);
+				}
+			}, maxNum / 2000);
+		});
+	}
+}, {});
+
+staticsCounter.observe(container);
+// animation on scroll
+let skillSection = document.querySelector("[data-skill]");
+let progress = document.querySelectorAll("[data-per]");
+
+let skillObserver = new IntersectionObserver((el) => {
+	el.forEach(
+		(el) => {
+			if (el.isIntersecting) {
+				let widthOne = 0;
+				let maxWidth = el.target.dataset.per;
+				let widthTimer = setInterval(() => {
+					widthOne++;
+					el.target.style.width = `${widthOne}%`;
+					if (el.target.style.width == maxWidth) {
+						clearInterval(widthTimer);
+						skillObserver.unobserve(el.target);
+					}
+				}, 1);
+			}
+		},
+		{ threshold: 1 }
+	);
+});
+
+progress.forEach((el) => {
+	skillObserver.observe(el);
+});
